3e0ceb793935addd3c3e932c4e1dbb11104f6543
mackowiakbella <isabellamdallabrida@gmail.com>
2024-03-21 16:10:53 -0400

    back-end I

---
+++ exerciciowhiledo01.js
@@ -0,0 +1,17 @@
+import entradaDados from 'readline-sync';
+
+const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+
+let tentativa;
+
+do {
+    tentativa = entradaDados.questionInt("Tente adivinhar o numero (entre 1 e 100): ");
+
+    if (tentativa === numeroAleatorio) {
+        console.log("Parabéns! Você adivinhou o número correto.");
+    } else if (tentativa < numeroAleatorio) {
+        console.log("Tente um número maior.");
+    } else {
+        console.log("Tente um número menor.");
+    }
+} while (tentativa !== numeroAleatorio);
